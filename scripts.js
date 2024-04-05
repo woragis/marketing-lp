4f3604c4bd36298595795652878d5acb96339c12
woragis <masteringthecode.woragis+github@gmail.com>
2024-04-04 23:13:06 -0300

    Format with prettier

--- scripts.js
+++ scripts.js
@@ -1,44 +1,47 @@
 function promoTimer() {
-  let timer = document.getElementById('timer')
+  let timer = document.getElementById("timer");
 
   let tempoLimite = 60 * 30; // 30 minutos
 
   setInterval(() => {
-    let hora = parseInt(tempoLimite / (60 * 60))
+    let hora = parseInt(tempoLimite / (60 * 60));
     let minutos = parseInt((tempoLimite / 60) % 60);
     let segundos = parseInt(tempoLimite % 60);
-    let displayTimer = `${hora}:${minutos}:${segundos}`
+    let displayTimer = `${hora}:${minutos}:${segundos}`;
     timer.innerHTML = displayTimer;
     if (tempoLimite > 0) tempoLimite--;
     else return;
   }, 1000);
 }
 
-promoTimer()
+promoTimer();
 
 function javascriptRender() {
   const conteudo = [
     {
-      title: 'Escalas', conteudo: [
-        { title: 'Maior', link: '' },
-        { title: 'Menor', link: '' },
-        { title: 'Pentatonica maior', link: '' },
-        { title: 'Pentatonica m7', link: '' }
-      ]
+      title: "Escalas",
+      conteudo: [
+        { title: "Maior", link: "" },
+        { title: "Menor", link: "" },
+        { title: "Pentatonica maior", link: "" },
+        { title: "Pentatonica m7", link: "" },
+      ],
     },
     {
-      title: 'Arpeggios', conteudo: [
-        { title: 'Maiores', link: '' },
-        { title: 'Menores', link: '' }
-      ]
-    }
-  ]
+      title: "Arpeggios",
+      conteudo: [
+        { title: "Maiores", link: "" },
+        { title: "Menores", link: "" },
+      ],
+    },
+  ];
 
-  const conteudoDiv = document.getElementById('conteudo')
+  const conteudoDiv = document.getElementById("conteudo");
   conteudoDiv.innerHTML = conteudo.map((tipo) => {
-    let assuntos = tipo.conteudo.map((assunto) => "<li><a href=" + assunto.link + ">" + assunto.title + "</a></li>")
-    return (
-      "<li><h4>" + tipo.title + "</h4><ol>" + assuntos[0] + "</ol></li>"
-    )
-  })
+    let assuntos = tipo.conteudo.map(
+      (assunto) =>
+        "<li><a href=" + assunto.link + ">" + assunto.title + "</a></li>"
+    );
+    return "<li><h4>" + tipo.title + "</h4><ol>" + assuntos[0] + "</ol></li>";
+  });
 }
